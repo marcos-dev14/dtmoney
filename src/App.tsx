3496947ce0d3449d957b376754bc31d898f6e6fd
marcos-dev14 <mprogramador.10@gmail.com>
2021-06-05 21:02:48 -0300

    feat: Consumindo API com o MirageJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { createServer } from 'miragejs';
+
 import { GlobalStyles } from "./styles/global";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
